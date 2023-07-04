39c5ea53eac7553909874f02833b3e3328d83a36
mendacium <sakshamlinkedin@gmail.com>
2023-07-04 22:36:03 +0530

    1st project start

---
+++ project1/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './components/Navbar'
+
+
+
+function App() {
+  return (
+    <>
+       
+    <Navbar title="Project1"/>
+    </>
+  );
+}
+
+
+export default App;
