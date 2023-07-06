21b2a64901a3bec02b646d651f7ef6f8844ea9b6
mendacium <sakshamlinkedin@gmail.com>
2023-07-06 15:06:56 +0530

    used useState prop and built a text box that changes the input text to uppercase

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,14 +1,19 @@
 
 import './App.css';
 import Navbar from './components/Navbar'
+import TextForm from './components/TextForm'
+
 
 
 
 function App() {
   return (
     <>
-       
-    <Navbar title="Project1"/>
+
+      <Navbar title="Project1" about="hello" />
+      <div className="container">
+        <TextForm heading="Enter Text"/>
+      </div>
     </>
   );
 }
---
+++ project1/src/components/TextForm.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+
+
+export default function TextForm({heading}) {
+    const[text,setText] = useState('Enter text')
+
+    const changeToUpper = () =>{
+        let newText = text.toLocaleUpperCase()
+        setText(newText)
+    }
+
+    const handleOnChange = (event)=>{
+        // console.log("text")
+        setText(event.target.value)
+    }
+
+    const textOnClick = ()=> {
+        setText("")
+    }
+
+    return (
+        <div>
+            <div className="mb-3">
+                <h3>{heading}</h3>
+                {/* <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label> */}
+                <textarea value={text} onClick={textOnClick} onChange={handleOnChange} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <button className="btn btn-primary" onClick={changeToUpper}>Click to UpperCase</button>
+            </div>
+        </div>
+    )
+}
