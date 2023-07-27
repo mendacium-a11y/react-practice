43f099999dde060f71cae01f04e5d5357f41b85d
mendacium <sakshamlinkedin@gmail.com>
2023-07-27 23:27:15 +0530

    Starting and ending index using binSearch

--- project3/src/Components/Newsitem.jsx
+++ project3/src/Components/Newsitem.jsx
@@ -10,7 +10,7 @@ export class Newsitem extends Component {
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href="/news" className="btn btn-primary">Go somewhere</a>
+              <a href="/newsbites" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
       </div>
